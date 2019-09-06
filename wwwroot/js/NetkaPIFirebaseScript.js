cc8dc783a5792581944beb49a28187eb8ce74b3c
nitchapatl <45028503+nitchapatl@users.noreply.github.com>
2019-09-06 14:36:43 +0700

    add netka firebase script

---
+++ wwwroot/js/NetkaPIFirebaseScript.js
@@ -0,0 +1,15 @@
+﻿<script src="https://www.gstatic.com/firebasejs/6.6.0/firebase-app.js"></script>
+
+<script>
+    var firebaseConfig = {
+        apiKey: "AIzaSyDdO_rTBjBG5ni4NGelxLI3XtM_8rFkfaQ",
+    authDomain: "netkapifirebase.firebaseapp.com",
+    databaseURL: "https://netkapifirebase.firebaseio.com",
+    projectId: "netkapifirebase",
+    storageBucket: "",
+    messagingSenderId: "419123180799",
+    appId: "1:419123180799:web:c9ebce54cde31269ef48ca"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
