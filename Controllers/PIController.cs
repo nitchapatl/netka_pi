d94186c927bcc47003f744f07606b02a802f6680
nitchapatl <45028503+nitchapatl@users.noreply.github.com>
2019-09-06 10:20:59 +0700

    initial commit

---
+++ Controllers/PIController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using netka_pi.Models;
+
+namespace netka_pi.Controllers
+{
+    public class PIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add_PI()
+        {
+            return View();
+        }
+    }
+}
